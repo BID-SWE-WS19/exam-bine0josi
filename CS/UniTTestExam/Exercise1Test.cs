ffe6bf94638958e9623259e58d6840c2a462912a
bine0josi <josefine.lenox@gmx.at>
2019-12-12 15:51:42 +0100

    unit test written

---
+++ CS/UniTTestExam/Exercise1Test.cs
@@ -0,0 +1,35 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using BID.SWE.EXAM.Impl;
+using NUnit.Framework;
+
+namespace UniTTestExam
+{
+    [TestFixture]
+    public class Exercise1Test
+    {
+        private Exercise1 ex1 = new Exercise1();
+        [Test]
+        public void TestIfInstanceIsNotNull()
+        {
+            Assert.IsNotNull(this.ex1.Method1());
+        }
+
+        [Test]
+        public void TestIfInstanceIsCellPrinter()
+        {
+            /// sorry for awful test, could not imagen any better one, sorry....
+            try
+            {
+                CellPrinter print = ex1.Method1() as CellPrinter;
+                Assert.IsTrue(true);
+            }
+            catch (System.Exception)
+            {
+                Assert.IsTrue(false);
+            }
+        }
+    }
+}
---
+++ CS/UniTTestExam/Exercise2Test.cs
@@ -0,0 +1,44 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using BID.SWE.EXAM.Impl;
+using NUnit.Framework;
+
+namespace UniTTestExam
+{
+    [TestFixture]
+    public class Exercise2Test
+    {
+        private Exercise2 ex2 = new Exercise2();
+
+        [Test]
+        public void Method2TestIfDuplicationWorks()
+        {
+            Assert.AreEqual(2, this.ex2.Method2());
+        }
+
+        [Test]
+        public void Method2TestIfReturnValueIsNotOne()
+        {
+            Assert.AreNotEqual(1, this.ex2.Method2());
+        }
+
+        [Test]
+        public void Method3TestIfEven()
+        {
+            int neighbour = 4;
+
+            Assert.IsTrue(ex2.Method3(neighbour));
+        }
+
+
+        [Test]
+        public void Method3TestIfNotEven()
+        {
+            int neighbour = 3;
+
+            Assert.IsFalse(ex2.Method3(neighbour));
+        }
+    }
+}
