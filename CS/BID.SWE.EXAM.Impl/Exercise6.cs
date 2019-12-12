e5e5713a501d4685a125f970c1d2c6f7a424be85
bine0josi <josefine.lenox@gmx.at>
2019-12-12 17:02:02 +0100

    final commit and push, hope everthing is fine

--- CS/BID.SWE.EXAM.Impl/Exercise6.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise6.cs
@@ -8,18 +8,25 @@ namespace BID.SWE.EXAM.Impl
     {
         public object Method1()
         {
+            int id = 34;
+            int tag = 12;
+            int monat = 12;
+            int jahr = 2019;
+            int stunde = 17;
+            int minute = 10;
+            string name = "Philippine";
             string query = "INSERT INTO TestAbgaben (Id, Tag, Monat, Jahr, Stunde, Minute, Name) " +
-                   "VALUES (@ID, @Tag, @Monat, @Jahr, @Stunde, @Minute, @Jahr) ";
+                   "VALUES (@ID, @Tag, @Monat, @Jahr, @Stunde, @Minute, @Jahr, @Name) ";
 
                 using (SqlCommand cmd = new SqlCommand(query))
                 {
-                    cmd.Parameters.AddWithValue("@ID", 34);
-                    cmd.Parameters.AddWithValue("@Tag", 12);
-                    cmd.Parameters.AddWithValue("@Monat", 12);
-                    cmd.Parameters.AddWithValue("@Jahr", 2019);
-                    cmd.Parameters.AddWithValue("@Stunde", 16);
-                    cmd.Parameters.AddWithValue("@Minute", 40);
-                    cmd.Parameters.AddWithValue("@Name", "Philippine");
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@Tag", tag);
+                    cmd.Parameters.AddWithValue("@Monat", monat);
+                    cmd.Parameters.AddWithValue("@Jahr", jahr);
+                    cmd.Parameters.AddWithValue("@Stunde", stunde);
+                    cmd.Parameters.AddWithValue("@Minute", minute);
+                    cmd.Parameters.AddWithValue("@Name", name);
 
                     return cmd;
                 }
