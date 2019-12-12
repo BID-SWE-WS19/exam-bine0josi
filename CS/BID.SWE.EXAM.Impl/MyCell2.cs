d519da09e9eb2025ca05cfa7a3a18c9a75e922bb
bine0josi <josefine.lenox@gmx.at>
2019-12-12 15:38:44 +0100

    implemented exercise 2, but ex1 and ex2 do not have unit test so far

---
+++ CS/BID.SWE.EXAM.Impl/MyCell2.cs
@@ -0,0 +1,15 @@
+﻿using BID.SWE1.Exam.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BID.SWE.EXAM.Impl
+{
+    public class MyCell2 : Cell
+    {
+        public override int Grow()
+        {
+            return this._size * 2;
+        }
+    }
+}
---
+++ CS/BID.SWE.EXAM.Impl/MyCell3.cs
@@ -0,0 +1,22 @@
+﻿using BID.SWE1.Exam.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BID.SWE.EXAM.Impl
+{
+    public class MyCell3 : Cell
+    {
+        public bool IsAlive(int neighbour)
+        {
+            if (neighbour%2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
