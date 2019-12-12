4187a644c983e8d5c1ac3881d359c06738cecf73
bine0josi <josefine.lenox@gmx.at>
2019-12-12 16:59:06 +0100

    added tests to exercise 4, one is not running through, have issues with streams

---
+++ CS/UniTTestExam/Exercise4Test.cs
@@ -0,0 +1,34 @@
+﻿
+namespace UniTTestExam
+{
+    using BID.SWE.EXAM.Impl;
+    using NUnit.Framework;
+    using System;
+    using System.IO;
+
+    [TestFixture]
+    public class Exercise4Test
+    {
+        private Exercise4 ex4 = new Exercise4();
+
+        [Test]
+        public void Method1TestIfReturnValueIsNotNull()
+        {
+            Assert.IsNotNull(ex4.Method1());
+        }
+
+
+        [Test]
+        public void Method2TestIfStringIsCorrect()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.Write("Hallo");
+
+            writer.Close();
+
+            Assert.IsNotNull(ex4.Method2(stream));
+        }
+    }
+}
