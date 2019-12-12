7c74dfdf93af302d8856a35004375917ee9eb270
bine0josi <josefine.lenox@gmx.at>
2019-12-12 16:07:48 +0100

    have some issues with the unit tests, care about it later

---
+++ CS/UniTTestExam/Exercise3Test.cs
@@ -0,0 +1,48 @@
+﻿using BID.SWE.EXAM.Impl;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniTTestExam
+{
+    [TestFixture]
+    class Exercise3Test
+    {
+        private Exercise3 ex3 = new Exercise3();
+
+        [Test]
+        public void Method1TestIfReturnIsNotNull()
+        {
+            Assert.IsNotNull(ex3.Method1());
+        }
+
+        [Test]
+        public void Method1TestIfReturnIsMyMap()
+        {
+            try
+            {
+                MyMap print = ex3.Method1() as MyMap;
+                Assert.IsTrue(true);
+            }
+            catch (System.Exception)
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        [Test]
+        public void Method1TestIfReturnIsSomethingElse()
+        {
+            try
+            {
+                CellPrinter print = ex3.Method1() as CellPrinter;
+                Assert.IsTrue(false);
+            }
+            catch (System.Exception)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
