197856e5772d9ac555c7a3cc4d7d6b84c0c5650e
bine0josi <josefine.lenox@gmx.at>
2019-12-12 16:29:37 +0100

    implemented exercsie 6

--- CS/BID.SWE.EXAM.Impl/Exercise6.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise6.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using BID.SWE1.Exam.Interfaces;
 
 namespace BID.SWE.EXAM.Impl
@@ -7,7 +8,21 @@ namespace BID.SWE.EXAM.Impl
     {
         public object Method1()
         {
-            throw new NotImplementedException();
+            string query = "INSERT INTO TestAbgaben (Id, Tag, Monat, Jahr, Stunde, Minute, Name) " +
+                   "VALUES (@ID, @Tag, @Monat, @Jahr, @Stunde, @Minute, @Jahr) ";
+
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@ID", 34);
+                    cmd.Parameters.AddWithValue("@Tag", 12);
+                    cmd.Parameters.AddWithValue("@Monat", 12);
+                    cmd.Parameters.AddWithValue("@Jahr", 2019);
+                    cmd.Parameters.AddWithValue("@Stunde", 16);
+                    cmd.Parameters.AddWithValue("@Minute", 40);
+                    cmd.Parameters.AddWithValue("@Name", "Philippine");
+
+                    return cmd;
+                }
         }
     }
 }
